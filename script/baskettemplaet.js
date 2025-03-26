477ad66ab23d76ba68ffa5aa8ee828f748cfe4b5
EisCore1986 <wassimmadwr@gmail.com>
2025-03-26 01:50:54 +0100

    Add initial implementation of basket template functions for handling basket operations

---
+++ script/baskettemplaet.js
@@ -0,0 +1,12 @@
+function toBasket(price, name) {
+  console.log(price, name);
+  isBasketExists();
+}
+
+function isBasketExists() {
+  // get ele secBasket
+}
+
+function name() {}
+function name() {}
+function createSectionBasket(params) {}
