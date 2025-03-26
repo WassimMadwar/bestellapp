7644d00fb7227cf01f8d2bbfe4367eeb8e917851
EisCore1986 <wassimmadwr@gmail.com>
2025-03-26 05:09:38 +0100

    Refactor basket template functions to improve article management and basket creation logic

--- script/baskettemplaet.js
+++ script/baskettemplaet.js
@@ -1,12 +1,89 @@
-function toBasket(price, name) {
-  console.log(price, name);
-  isBasketExists();
+let mod = "new";
+let update = "increase";
+
+// if it dosnt work  should use else !!!
+function toBasket(price, name, Id) {
+  if (!isBasketExists()) {
+    createSectionBasket();
+  }
+  addArticle(price, name, Id);
+  // else {
+  //   addArticle(price, name, Id);
+  // }
+}
+function addArticle(price, name, Id) {
+  if (!isArticleExists(Id)) {
+    createNewArticle(price, name, Id);
+  }
+  increaseOne(price, name, Id);
+  // else {
+  //   increaseOne(price, name, Id);
+  // }
 }
 
 function isBasketExists() {
-  // get ele secBasket
+  //  IF get ele secBasket
+  // return true;
 }
 
+function isArticleExists() {
+  // IF get ele
+  // return true;
+}
+function createSectionBasket() {
+  // const main = document.getElementById("main");
+  const secBasket = document.createElement("section");
+  secBasket.id = "secBasket";
+
+  const artBasket = createArtBasket();
+  const artKase = createArtKasse();
+
+  secBasket.append(artBasket, artKase);
+  // main.appendChild(secBasket);
+  return secBasket;
+}
+
+function createArtBasket() {
+  const artBasket = document.createElement("section");
+  artBasket.id = "artBasket";
+  const headerBasket = createHeaderBasket();
+  artBasket.appendChild(headerBasket);
+  return artBasket;
+}
+function createHeaderBasket() {
+  const headerBasket = document.createElement("header");
+  headerBasket.id = "headerBasket";
+  return headerBasket;
+}
+function create() {
+  return;
+}
+
+function createArtKasse() {
+  const artKase = document.createElement("section");
+  artKase.id = "artKase";
+
+  return artKase;
+}
+function createNewArticle(price, name, Id) {}
+
+function increaseOne(price, name, Id) {
+  let update = "increase";
+  updateAmountArticle(price, Id, update);
+}
+function decreaseOne(price) {
+  let update = "decrease";
+  updateAmountArticle(price, Id, update);
+}
+
+function updateAmountArticle(price, Id, update) {
+  updateTotalArticle(price, Id, update);
+}
+function updateTotalArticle(price, Id, update) {
+  updateSubtotal(price, update);
+}
+function updateSubtotal(price, update) {
+  updateTotalInvoice(price, update);
+}
+function updateTotalInvoice(price) {}
 function name() {}
-function name() {}
-function createSectionBasket(params) {}
